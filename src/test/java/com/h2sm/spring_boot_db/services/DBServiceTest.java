de1a96b4853596b626518117334775cb38869c75
Egor Fedorenko <fedore.egor@gmail.com>
2021-10-15 16:40:27 +0300

    some updates for tests

--- src/test/java/com/h2sm/spring_boot_db/services/DBServiceTest.java
+++ src/test/java/com/h2sm/spring_boot_db/services/DBServiceTest.java
@@ -1,18 +1,26 @@
 package com.h2sm.spring_boot_db.services;
+import com.h2sm.spring_boot_db.SpringBootDbApplication;
 import com.h2sm.spring_boot_db.config.SpringConfig;
 import com.h2sm.spring_boot_db.repository.interfaces.AchievementsRepo;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest(classes = SpringConfig.class)
-@Import(DBService.class)
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
 class DBServiceTest {
     TestData data = new TestData();
 
-    @Autowired
+    @MockBean
     AchievementsRepo achievements;
 
     @Test
@@ -21,6 +29,7 @@ class DBServiceTest {
 
     @Test
     void getAchievements() {
+        achievements.returnAllAchievements().forEach(System.out::println);
         Assertions.assertThat(data.returnAchievements()).containsExactlyElementsOf(achievements.returnAllAchievements().stream().toList());
     }
 
