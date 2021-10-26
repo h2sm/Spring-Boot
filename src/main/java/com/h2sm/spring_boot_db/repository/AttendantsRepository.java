d4ce6cf36cbbd0d821b409ecaf433e778264bb09
Egor Fedorenko <fedore.egor@gmail.com>
2021-10-27 00:19:07 +0300

    added a new repository

--- src/main/java/com/h2sm/spring_boot_db/repository/AttendantsRepository.java
+++ src/main/java/com/h2sm/spring_boot_db/repository/AttendantsRepository.java
@@ -2,12 +2,28 @@ package com.h2sm.spring_boot_db.repository;
 
 import com.h2sm.spring_boot_db.models.Attendant;
 import com.h2sm.spring_boot_db.repository.interfaces.AttendantsRepo;
-
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import java.util.Collection;
+import java.util.Map;
 
+@Repository
+@RequiredArgsConstructor
+@Component
 public class AttendantsRepository implements AttendantsRepo {
+    private final NamedParameterJdbcOperations jdbc;
+
     @Override
     public Collection<Attendant> getAllAttendants() {
-        return null;
+        return jdbc.query("""
+                select * from attendant;
+                """, Map.of("attendant_id", "attendant_name" + "tel_number"),
+                (rs,i) -> new Attendant(
+                        rs.getInt("attendant_id"),
+                        rs.getString("attendant_name"),
+                        rs.getString("tel_number")
+                ));
     }
 }
